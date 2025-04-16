c15cdd312caa607757e4b7362dc31ed7d5770a66
Wong Liyongertiopo <kakakadita25@gmail.com>
2025-04-16 09:43:44 +0700

    chore: update all frontend files and login feature

--- UI/src/utils/Endpoint.jsx
+++ UI/src/utils/Endpoint.jsx
@@ -1,7 +1,8 @@
 export const BASEURL = "https://irrregularshop-production.up.railway.app";
 
-export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
 export const URL_PRODUCT = `${BASEURL}/api/products`;
+
+export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
 export const URL_TRANSACTION = `${BASEURL}/api/transactions`;
 export const URL_BANNER = `${BASEURL}/api/banners`;
 export default BASEURL;
