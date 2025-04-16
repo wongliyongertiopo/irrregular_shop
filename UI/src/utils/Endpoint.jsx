17c795893db51bd28882bacfdca62af10de46d57
Wong Liyongertiopo <kakakadita25@gmail.com>
2025-04-16 09:19:07 +0700

    fix: update BASEURL ke production

--- UI/src/utils/Endpoint.jsx
+++ UI/src/utils/Endpoint.jsx
@@ -1,6 +1,7 @@
-export const BASEURL = "https://irrregularshop-production.up.railway.app";
+export const BASEURL = "https://irrregularshop-production.up.railway.app"; // ✅ ini benar
 
 export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
 export const URL_PRODUCT = `${BASEURL}/api/products`;
 export const URL_TRANSACTION = `${BASEURL}/api/transactions`;
 export const URL_BANNER = `${BASEURL}/api/banners`;
+export default BASEURL;
